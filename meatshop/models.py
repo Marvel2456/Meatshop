9ca9484728acdd73a9bdcbb36bbb8623a18218e8
Marvel2456 <marvellousidemudia@gmail.com>
2024-05-30 14:44:15 +0100

    finished up with the backend logic

--- meatshop/models.py
+++ meatshop/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    name = models.CharField(max_length=250, blank=True, null=True)
+    product_image = models.ImageField(upload_to='item/image', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.product_image.url
+        except:
+            url = ''
+        return url
+    
+class Contact(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    name = models.CharField(max_length=225, blank=True, null=True)
+    email = models.CharField(max_length=225, blank=True, null=True)
+    phone_number = models.CharField(max_length=225, blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.created_at}'
+
+class Branch(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    branch_name = models.CharField(max_length=250, blank=True, null=True)
+    branch_address = models.CharField(max_length=250, blank=True, null=True)
+    branch_image =  models.ImageField(upload_to='branch/image', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.branch_name
+
--- meatshop/views.py
+++ meatshop/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product, Contact
+from django.contrib import messages
 
 # Create your views here.
 
-
 def homeView(request):
-
-    return render(request, 'pages/home.html')
+    product = Product.objects.all()
+    return render(request, 'pages/home.html', {'product': product})
 
 def aboutView(request):
     return render(request, 'pages/about.html')
 
 def contactView(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone_number = request.POST['phone_number']
+        message = request.POST['message']
+
+        contact_us = Contact(name=name, email=email, phone_number=phone_number, message=message)
+
+        contact_us.save()
+
+        messages.success(request, 'Your request has been submitted thank you')
+        return redirect('contact')
     return render(request, 'pages/contact.html')
 
 def productView(request):
-    return render(request, 'pages/products.html')
+    product = Product.objects.all()
+    return render(request, 'pages/products.html', {'product': product})
